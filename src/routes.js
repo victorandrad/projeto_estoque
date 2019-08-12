a2d82b4991ce18800b53aa8ca15703a9f43f52e9
Victor Andrade <victor.ferreira@nanoincub.com.br>
2019-08-12 18:13:09 -0300

    Versão 1.0 do congelado

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,18 @@ const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+routes.options("/*", function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+});
+
+routes.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store); 
 routes.post('/devs/:devId/likes', LikeController.store);
