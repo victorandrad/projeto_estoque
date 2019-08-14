9c6c08d7a0ded86222f71ed61642323220aeb01c
Victor Andrade <victor.ferreira@nanoincub.com.br>
2019-08-14 20:35:56 -0300

    Adiciona cors no servidor

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 
 const routes = require('./routes');
 const server = express();
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://dev:dev01@cluster0-edysn.mongodb.net/dev?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
